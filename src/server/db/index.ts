3a51fad5eba354518d1367edcec5c31146b440ee
David Bell-Brown <david.bellbrown@roksoftware.com>
2025-01-03 19:57:18 -0800

    possibly unnecessary dev db cache

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,13 @@
 import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import { neon, type NeonQueryFunction } from "@neondatabase/serverless";
 import { env } from "~/env";
 import * as schema from "./schema";
 
-const sql = neon(env.DATABASE_URL);
+const globalForDb = globalThis as unknown as {
+  conn: NeonQueryFunction<false, false>;
+};
+
+const sql = globalForDb.conn ?? neon(env.DATABASE_URL);
+if (process.env.NODE_ENV !== "production") globalForDb.conn = sql;
+
 export const db = drizzle(sql, { schema });
